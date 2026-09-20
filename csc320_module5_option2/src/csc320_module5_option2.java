622c3d331ac150358dd140d8236eda567324ca42
Alex Foster <alexander.foster@csuglobal.edu>
2026-09-20 08:25:04 -0600

    main selection options, calc yearly average

--- csc320_module5_option2/src/csc320_module5_option2.java
+++ csc320_module5_option2/src/csc320_module5_option2.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 
 public class csc320_module5_option2 {
 	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
 		//create array named "monthNames" with each month's name included
 		String[] monthNames = {"January", "February", "March","April",
 				"May", "June", "July", "August",
@@ -18,13 +19,71 @@ public class csc320_module5_option2 {
 		};
 		
 		//create array with each month's average temperature included
-		int[] monthlyAverageTemp = {50, 55, 60, 65, 70, 75, 80, 75, 70, 65, 60, 55};
+		int[] monthlyAverageTemps = {58, 55, 60, 65, 70, 75, 80, 75, 70, 65, 60, 55};
+
+		double yearlyAverage = monthlyAverageTemps[0];		//Yearly average of monthly temperatures
+		double maxMonthlyAverage = monthlyAverageTemps[0];	//maximum monthly average temperature, initialized to January
+		double minMonthlyAverage = monthlyAverageTemps[0];	//minimum monthly average temperature, initialized to January
+		double tempYearTotal = 0;							//temp variable to calculate yearly average
 		
+		int i;	//iteration variable
+		
+		//Print program instructions to console.
 		System.out.println("Instructions:");
 		System.out.println("Enter the name of the month you want to konw the average temperature of.");
-		System.out.println("Enter \"year\" to get the average temperature for all months along with yearly average ");
-		System.out.println("	yearly average temperature and highest and lowest monthly averages.");
-
+		System.out.println("Enter \"Year\" to get the average temperatures for all months along with yearly average temperature and highest and lowest monthly averages.");
+		System.out.println("");
+		System.out.println("Enter one of the following:");
+		for (i = 0; i < monthNames.length; ++i) { //Print all elements in monthNames
+			System.out.print("\"" + monthNames[i] + "\", ");
+		}
+		System.out.println("\"Year\"");
+		//System.out.println("Enter \"Exit\" to quit.");
 		
+		//Get user input
+		String userSelection;
+		
+		userSelection = scnr.next();		
+
+		//for loop through month names only. Year as a separate loop.
+		for (i = 0; i < monthNames.length; ++i) {
+			//for loop that selects the element chosen, outputs month and average temperature
+			if (userSelection.equals(monthNames[i])) {
+				System.out.println("You entered \"" + monthNames[i] + "\"");
+				System.out.println("The month's average temperature is: " + monthlyAverageTemps[i]);
+			}
+		}
+		
+		//check if user input "Year"
+		if (userSelection.equals("Year")) {
+			System.out.println ("You entered \"Year\"");
+			//for loop printing all months and year average
+			for (i = 0; i < monthNames.length; ++i) {
+				//outputs all months and average temperature
+				System.out.println(monthNames[i] + ": " + monthlyAverageTemps[i]);
+			}
+			
+			//sum monthly averages into tempYearTotal, set max, set min
+			for (i = 0; i < monthlyAverageTemps.length; ++i) {
+				tempYearTotal += monthlyAverageTemps[i];
+			}
+			
+			//Set yearlyAverage to year total divided by 12
+			yearlyAverage = tempYearTotal / 12;
+			
+			System.out.print("Yearly average: ");
+			System.out.printf("%.1f%n", yearlyAverage);
+			System.out.println("");
+			
+			//
+			
+			
+		}
+			
+			/*else {
+				System.out.println("You entered \"" + userSelection +"\". Please enter a month or \"Year\"");
+				break;
+			}*/
+			//
 	}	
 }
